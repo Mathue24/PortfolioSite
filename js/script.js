b9481031535e75e1ded89397a6aaef6ff55e5b2e
Mathue24 <Mathue24@gmail.com>
2019-02-28 11:29:09 +0100

    cleanup

---
+++ js/script.js
@@ -0,0 +1,36 @@
+function lemonFunction() {
+	var fruit = document.getElementById("corgiLemon");
+	var y = document.getElementById("corgiPumpkin");
+	var z = document.getElementById("corgiNutella");
+	if (fruit.style.display === "block") {
+		fruit.style.display = "none";
+	} else {
+		fruit.style.display = "block";
+		y.style.display = "none";
+		z.style.display = "none";
+	}
+}
+function pumpkinFunction() {
+	var fruit = document.getElementById("corgiPumpkin");
+	var y = document.getElementById("corgiLemon");
+	var z = document.getElementById("corgiNutella");
+	if (fruit.style.display === "block") {
+		fruit.style.display = "none";
+	} else {
+		fruit.style.display = "block";
+		y.style.display = "none";
+		z.style.display = "none";
+	}
+}
+function nutellaFunction() {
+	var fruit = document.getElementById("corgiNutella");
+	var y = document.getElementById("corgiPumpkin");
+	var z = document.getElementById("corgiLemon");
+	if (fruit.style.display === "block") {
+		fruit.style.display = "none";
+	} else {
+		fruit.style.display = "block";
+		y.style.display = "none";
+		z.style.display = "none";
+	}
+}
