d212d914bde2d6835ab29a14e1565aec1b6f0414
Mathue24 <Mathue24@gmail.com>
2019-03-07 00:43:53 +0100

    cleanup

--- js/script.js
+++ js/script.js
@@ -1,36 +1,14 @@
-function lemonFunction() {
-	var fruit = document.getElementById("corgiLemon");
-	var y = document.getElementById("corgiPumpkin");
-	var z = document.getElementById("corgiNutella");
-	if (fruit.style.display === "block") {
-		fruit.style.display = "none";
+function doggoFunction(doggo, first, second) {
+	var doggo = document.getElementById(doggo);
+	var first = document.getElementById(first);
+	var second = document.getElementById(second);
+	console.log("First:"+doggo);
+	parseInt
+	if (doggo.style.display === "block") {
+		doggo.style.display = "none";
 	} else {
-		fruit.style.display = "block";
-		y.style.display = "none";
-		z.style.display = "none";
-	}
-}
-function pumpkinFunction() {
-	var fruit = document.getElementById("corgiPumpkin");
-	var y = document.getElementById("corgiLemon");
-	var z = document.getElementById("corgiNutella");
-	if (fruit.style.display === "block") {
-		fruit.style.display = "none";
-	} else {
-		fruit.style.display = "block";
-		y.style.display = "none";
-		z.style.display = "none";
-	}
-}
-function nutellaFunction() {
-	var fruit = document.getElementById("corgiNutella");
-	var y = document.getElementById("corgiPumpkin");
-	var z = document.getElementById("corgiLemon");
-	if (fruit.style.display === "block") {
-		fruit.style.display = "none";
-	} else {
-		fruit.style.display = "block";
-		y.style.display = "none";
-		z.style.display = "none";
+		doggo.style.display = "block";
+		first.style.display = "none";
+		second.style.display = "none";
 	}
 }
